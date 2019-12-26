6c0a3225f3c02a803025e837dbe8f17974db92b6
Peregurin <chinmayvirmani@gmail.com>
2019-12-26 21:35:27 +0530

    Adding Unity AI
    Not complete yet

---
+++ John_Lemon's_Haunted_Jaunt/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour {
+    public float fadeDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+    float m_timer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+    private void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+    void EndLevel()
+    {
+        m_timer += Time.deltaTime;
+    }
+}
